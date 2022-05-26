4d14fd9a537bdd5da404c71df87e529af94a1f16
eedelosreyes2 <delosreyes.elijah@gmail.com>
2022-05-25 21:56:50 -0700

    nac3221

--- pages/components/intro.js
+++ pages/components/intro.js
@@ -4,8 +4,8 @@ export default function Intro() {
   return (
     <div className="flex items-center py-16">
       <Image
-        src="https://pbs.twimg.com/profile_images/1521267985154936832/wHliysQK_400x400.jpg"
-        alt="elijahdr.eth"
+        src="/NAC3221.gif"
+        alt="elijahdr"
         className="rounded-full"
         height={100}
         width={100}
